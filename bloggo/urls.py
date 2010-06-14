966f8bbc0022469c7d1225627658a84d4f3f0754
Kun Xi <kunxi@kunxi.org>
2010-06-14 02:07:47 -0700

    Add feed support.

--- bloggo/urls.py
+++ bloggo/urls.py
@@ -1,5 +1,16 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+from basic.blog.feeds import BlogPostsFeed, BlogPostsByCategory
+import tests
+
+feeds = {
+    'latest': BlogPostsFeed,
+    'categories': BlogPostsByCategory,
+}
 
 urlpatterns = patterns('',
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict' : feeds}),
+    url(r'siteconf', view=tests.siteconf_view, name='test_siteconf'),
     (r'', include('basic.blog.urls')),
 )
+
